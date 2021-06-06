19e447f8023fc50a13472f67e8b48eddb8ccd76b
andre100500 <39929065+andre100500@users.noreply.github.com>
2021-06-06 13:35:03 +0300

    Initial commit

---
+++ Library/Models/Catalog/AssetDetailModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models.Catalog
+{
+    public class AssetDetailModel
+    {
+        public int AssetId { get; set; }
+        public string Titel { get; set; }
+        public string AuthorOrDirector { get; set; }
+        public int Year { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
+        public string Isbn { get; set; }
+        public string Dewey { get; set; }
+        public decimal Cost { get; set; }
+        public string CurrentLocation { get; set; }
+        public string ImageUrl { get; set; }
+        public string PatronName { get; set; }
+        
+        public IEnumerable<AssetHoldModel> CurrentHolds { get; set; }
+    }
+}
