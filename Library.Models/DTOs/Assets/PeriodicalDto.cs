e6c03fd30d9469d61d5abb790e8991c3ac439eba
andre100500 <39929065+andre100500@users.noreply.github.com>
2021-07-05 22:06:00 +0300

    Fix

---
+++ Library.Models/DTOs/Assets/PeriodicalDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models.DTOs.Assets
+{
+    public class PeriodicalDto
+    {
+        public int Id { get; set; }
+        public string AssetId { get; set; }
+        public string UniformTitle { get; set; }
+        public string Publisher { get; set; }
+        public string ISSN { get; set; }
+        public DateTime DateOfPublication { get; set; }
+        public string Description { get; set; }
+        public string Language { get; set; }
+        public LibraryAssetDto Asset { get; set; }
+    }
+}
